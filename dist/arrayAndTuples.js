9980fafdd4517e243443e49c9ce7989a6b5a0ac9
codernoyan <coder.noyan@gmail.com>
2022-12-01 02:41:48 +0600

    explored tuples

--- dist/arrayAndTuples.js
+++ dist/arrayAndTuples.js
@@ -1,9 +1,17 @@
 "use strict";
 // let players: string[] = ["Messi", "Ronaldo"];
+// let players: (string | number | boolean)[];
+// let players: string[] | number[] | boolean[];
+// tuples
 var players;
-players = ["Messi", "Ronaldo"];
+players = ["Messi", "Ronaldo", 25, true];
+// players = [true, false];
+// players = [256, 512];
+players[0] = "Dybala";
+// players[1] = 25
+players[2] = 66;
 // players = false;
-players[0] = 'Dybala';
+// players[0] = 'Dybala';
 // players.push(58, false)
 // players.push("Neymar");
 // players.push(256);
--- src/arrayAndTuples.ts
+++ src/arrayAndTuples.ts
@@ -1,9 +1,20 @@
 // let players: string[] = ["Messi", "Ronaldo"];
-let players: (string | number | boolean)[];
-players = ["Messi", "Ronaldo"];
+// let players: (string | number | boolean)[];
+// let players: string[] | number[] | boolean[];
 
+// tuples
+let players: [string, string, number, boolean]
+
+players = ["Messi", "Ronaldo", 25, true];
+
+// players = [true, false];
+
+// players = [256, 512];
+players[0] = "Dybala";
+// players[1] = 25
+players[2] = 66;
 // players = false;
-players[0] = 'Dybala';
+// players[0] = 'Dybala';
 // players.push(58, false)
 
 // players.push("Neymar");
